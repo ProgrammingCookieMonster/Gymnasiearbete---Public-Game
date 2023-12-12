58d4f999e53217d361531f6a25aee77956c765ac
Andrei Bors <andreibors2003@gmail.com>
2023-12-12 20:35:57 +0100

    Adding last positioning and graphics.
    - the background has to be fixed next time and the speed/syncronisations of the objects (game balancing)

--- projektFrogger/frogger_main.py
+++ projektFrogger/frogger_main.py
@@ -5,10 +5,11 @@ import math
 
 #Set up the screen
 wn = turtle.Screen()
-wn.cv._rootwindow.resizable(False, False)
 wn.title("Frogger by Andrei")
-wn.setup = (600, 800)
-wn.bgcolor("black")
+wn.setup(600, 800) # width x height
+wn.cv._rootwindow.resizable(False, False)
+wn.bgcolor("green")
+wn.bgpic("graphics/others/frogger_background.gif")
 wn.tracer(0)
 
 #Shapes registration
@@ -19,7 +20,8 @@ shapes = [
         "graphics/others/turtles_left_half.gif", "graphics/others/turtles_left_submerged.gif",
         "graphics/others/turtles_right_submerged.gif", "graphics/cars/racing_car1_left.gif",
         "graphics/cars/racing_car2_left.gif", "graphics/cars/police_car_left.gif",
-        "graphics/cars/truck_right.gif", "graphics/cars/truck_left.gif", "graphics/others/croc_killer.gif"
+        "graphics/cars/truck_right.gif", "graphics/cars/truck_left.gif", "graphics/others/croc_killer.gif",
+        "graphics/others/goal.gif", "graphics/others/frog_is_home.gif"
         ]
 for shape in shapes:
     wn.register_shape(shape)
@@ -27,6 +29,7 @@ for shape in shapes:
 pen = turtle.Turtle()
 pen.speed(0)
 pen.hideturtle()
+pen.penup()
 
 #Classes
 class Sprite(): # sprite = character
@@ -41,6 +44,9 @@ class Sprite(): # sprite = character
         pen.shape(self.image)
         pen.stamp()
 
+    def update(self):
+        pass
+
     def is_collision(self, other):
         x_collosion = (math.fabs(self.x - other.x) * 2) < (self.width + other.width)
         y_collision = (math.fabs(self.y - other.y) * 2) < (self.height + other.height)
@@ -50,6 +56,7 @@ class Player(Sprite):
     def __init__(self, x, y, width, height, image):
         Sprite.__init__(self, x, y, width, height, image)
         self.dx = 0
+        self.collision = False
     def up(self):
         self.y += 50
     def down(self):
@@ -89,19 +96,10 @@ class Log(Sprite):
         self.x += self.dx
 
         #Space Checking - borders
-        if self.x < -500:
-            self.x = 500
-        if self.x > 500:
-            self.x = -500
-
-class Crocodile(Sprite):
-    def __init__(self, x, y, width, height, image, dx): #Dx stands for Delta x, changing the direction
-        Sprite.__init__(self, x, y, width, height, image)
-        self.dx = dx
-    def update(self):
-        self.x += self.dx
-
-
+        if self.x < -550:
+            self.x = 550
+        if self.x > 550:
+            self.x = -550
 
 class Turtle(Sprite):
     def __init__(self, x, y, width, height, image, dx): #Dx stands for Delta x, changing the direction
@@ -149,29 +147,43 @@ class Turtle(Sprite):
             self.state = "full"
             self.start_time = time.time()
 
+class Home(Sprite):
+    def __init__(self, x, y, width, height, image):
+        Sprite.__init__(self, x, y, width, height, image)
+        self.dx = 0
+
 
 #Objects --> Objects won't be in row others as there is a certain variation of positions; creating game pattern
 player = Player(0, -300, 40, 40, "graphics/sprite_individuals/frog_frontv1.gif")
 # CARS
-car_left = Car(300, -250, 121, 40, "graphics/cars/car1_left.gif", -2.5) # Row 1
-car_right = Car(-300, -100, 121, 40, "graphics/cars/car1_right.gif", +2.5) # Row 4
-racing_car1 = Car(-300, -150, 121, 40, "graphics/cars/racing_car1_left.gif", -4.0) # Row 3
-racing_car2 = Car(-450, -150, 121, 40, "graphics/cars/racing_car2_left.gif", -4.0) # Row 3 --> Racing cars and police car are racing
-racing_car3 = Car(-540, -150, 121, 40, "graphics/cars/police_car_left.gif", -3.0) # Row 3
-truck_left = Car(300, -50, 200, 40, "graphics/cars/truck_left.gif", -2.5) # Row 5
-truck_right = Car(300, -200, 200, 40, "graphics/cars/truck_right.gif", +2.5) # Row 2
+car_left = Car(300, -250, 121, 40, "graphics/cars/car1_left.gif", -2.5) # Row 2
+car_right = Car(-300, -100, 121, 40, "graphics/cars/car1_right.gif", +2.5) # Row 5
+racing_car1 = Car(-300, -150, 121, 40, "graphics/cars/racing_car1_left.gif", -4.0) # Row 4
+racing_car2 = Car(-450, -150, 121, 40, "graphics/cars/racing_car2_left.gif", -4.0) # Row 4 --> Racing cars and police car are racing
+racing_car3 = Car(-540, -150, 121, 40, "graphics/cars/police_car_left.gif", -3.0) # Row 4
+truck_left = Car(300, -50, 200, 40, "graphics/cars/truck_left.gif", -2.5) # Row 6
+truck_right = Car(300, -200, 200, 40, "graphics/cars/truck_right.gif", +2.5) # Row 3
+# Row 7 - safe zone - shores
 # LOGS
-log_left = Log(-300, 50, 100, 40, "graphics/logs/log_full.gif", -2.5) # Row 6
-log_right = Log(-300, 150, 100, 40, "graphics/logs/log_full.gif", +2.5) #
+log_left = Log(-300, 50, 100, 40, "graphics/logs/log_full.gif", +2.45) # Row 8
+log_right = Log(-300, 150, 100, 40, "graphics/logs/log_full.gif", +1.75) # Row 10
+log_left2 = Log(-300, 250, 100, 40, "graphics/logs/log_full.gif", +3.2) # Row 12
 # TURTLES
-turtle_left = Turtle(-300, 100, 200, 32, "graphics/others/turtles_left.gif", -1.0) # Row 7
-turtle_right = Turtle(-300, 200, 250, 32, "graphics/others/turtles_right.gif", +1.0)
-killer = Crocodile(400, 100, 100, 40, "graphics/others/croc_killer.gif", -1.0)
+turtle_left = Turtle(-300, 100, 200, 32, "graphics/others/turtles_left.gif", -1.8) # Row 9
+turtle_right = Turtle(-300, 200, 250, 32, "graphics/others/turtles_right.gif", -1.2) # Row 11
+killer = Car(400, 100, 100, 40, "graphics/others/croc_killer.gif", +1.9) # Row 9 - The croc may sometimes attach to the turtles, makes the game harder for the player
+# GOAL - player must get here to win --> All homes are on Row 13
+home1 = Home(0, 300, 50, 50, "graphics/others/goal.gif")
+home2 = Home(-100, 300, 50, 50, "graphics/others/goal.gif")
+home3 = Home(-200, 300, 50, 50, "graphics/others/goal.gif")
+home4 = Home(100, 300, 50, 50, "graphics/others/goal.gif")
+home5 = Home(200, 300, 50, 50, "graphics/others/goal.gif")
 
 #List of Objects
 sprites = [
-        car_left, car_right, log_left, log_right,
-        turtle_right, turtle_left, racing_car1, racing_car2, racing_car3, truck_right, truck_left, killer, player
+        car_left, car_right, log_left, log_right, log_left2,
+        turtle_right, turtle_left, racing_car1, racing_car2, racing_car3, truck_right, truck_left, killer, home1,
+        home2, home3, home4, home5, player
         ] # Creating a list to minimize the further code
 
 # Keyboard binding --> controlls
@@ -187,23 +199,31 @@ while True:
         sprite.render(pen) #Render objects
         sprite.update() #Update objects
 
-    player.dx = 0
+    player.dx = 0 # Checking for collisions
+    player.collision = False
     for sprite in sprites:
         if player.is_collision(sprite):
             if isinstance(sprite, Car): #Collisions with cars --> Frog dies
                 player.x = 0
                 player.y = -300
                 break
-            elif isinstance(sprite, Crocodile):
-                player.x = 0
-                player.y = -300
             elif isinstance(sprite, Log): #Collisions with log --> Frog floats
                 player.dx = sprite.dx
+                player.collision = True
                 break
             elif isinstance(sprite, Turtle) and sprite.state != "submerged": #Collision with turtles --> Frog floats only on full and half state
                 player.dx = sprite.dx
+                player.collision = True
                 break
-
+            elif isinstance(sprite, Home):
+                player.x = 0
+                player.y = -300
+                sprite.image = "graphics/others/frog_is_home.gif"
+                break
+        # Check the player is/isn't touching the water (y > 0 - above the safe line)
+    if player.y > 0 and player.collision != True: # ADD SOUND OF WHATER SPLASH WHEN PLAYER DIES
+        player.x = 0
+        player.y = -300
 
     #Update Screen
     wn.update()

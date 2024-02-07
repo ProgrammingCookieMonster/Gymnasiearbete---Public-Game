de072921dda3c7d43867a450a0c4931763746a09
Andrei Bors <andreibors2003@gmail.com>
2024-02-07 16:23:01 +0100

    Working healing spell and consistancy

--- projektZelda/code/level.py
+++ projektZelda/code/level.py
@@ -3,11 +3,13 @@ from settings import *
 from tile import Tile
 from player import PLayer
 from support import *
-from random import choice
+from random import choice, randint
 from weapon import Weapon
 from debug import debug
 from ui import UI
 from enemy import Enemy
+from particles import AnimationPlayer
+from magic import MagicPlayer
 
 class Level:
     def __init__(self):
@@ -29,6 +31,11 @@ class Level:
 
          # user interface
         self.ui = UI()
+
+        # particles
+        self.animation_player = AnimationPlayer()
+        self.magic_player = MagicPlayer(self.animation_player)
+
     def create_map(self):
         layouts = {
             'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
@@ -74,14 +81,21 @@ class Level:
                                     (x, y),
                                     [self.visible_sprites, self.attackable_sprites],
                                     self.obstacle_sprites,
-                                    self.damage_player)
+                                    self.damage_player,
+                                    self.trigger_death_particles)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
-    def create_magic(self, style, strenght, cost):
-        print(style)
-        print(strenght)
+    def create_magic(self, style, strength, cost):
+        if style == 'heal':
+            self.magic_player.heal(self.player, strength, cost, [self.visible_sprites])
+
+        if style == 'flame':
+            pass
+
+        # print(style)
+        print(strength)
         print(cost)
 
     def destroy_attack(self):
@@ -94,7 +108,7 @@ class Level:
             self.player.health -= amount
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
-            # spawn particles
+            self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
 
     def player_attack_logic(self):
         if self.attack_sprites:
@@ -103,9 +117,16 @@ class Level:
                 if collision_sprites:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
+                            pos = target_sprite.rect.center
+                            offset = pygame.math.Vector2(0, 60)
+                            for leaf in range(randint(3, 6)):
+                                self.animation_player.create_grass_particles(pos - offset, [self.visible_sprites])
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player, attack_sprites.sprite_type)
+
+    def trigger_death_particles(self, pos, particle_type):
+        self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
 
     def run(self):
         # update and draw the game
---
+++ projektZelda/code/magic.py
@@ -0,0 +1,20 @@
+import pygame
+from settings import *
+
+class MagicPlayer:
+    def __init__(self, animation_player):
+        self.animation_player = animation_player
+
+    def heal(self, player, strength, cost, groups):
+        if player.energy >= cost:
+            player.health += strength
+            player.energy -= cost
+            offset = pygame.math.Vector2(0, -35)
+            if player.health >= player.stats['health']:
+                player.health = player.stats['health']
+            self.animation_player.create_particles('aura', player.rect.center, groups)
+            self.animation_player.create_particles('heal', player.rect.center + offset, groups)
+
+    def flame(self, player, cost, groups):
+        if player.energy >= cost:
+            player.energy -= cost
